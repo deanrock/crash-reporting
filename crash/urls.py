ace265b049812f6e1e9159a078d5217b93469aa2
Dejan Levec <dejan@dejanlevec.com>
2015-07-04 12:07:10 +0200

    new android path

--- crash/urls.py
+++ crash/urls.py
@@ -6,7 +6,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'crash.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'acra/_design/acra-storage/_update/report/', 'reporting.views.report_android', name='report-android'),
+    url(r'report/android/', 'reporting.views.report_android', name='report-android'),
+    url(r'acra/_design/acra-storage/_update/report/', 'reporting.views.report_android', name='report-android-legacy'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
